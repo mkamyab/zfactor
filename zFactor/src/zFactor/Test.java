41d9cf73d1f64163c88607809f49dfb32420732a
Mohammadreza Kamyab <m@kamyab.co>
2015-07-12 16:09:27 +0800

    Adding ANN5 and DAK methods

--- zFactor/src/zFactor/Test.java
+++ zFactor/src/zFactor/Test.java
@@ -3,13 +3,33 @@ package zFactor;
 public class Test {
 
 	public static void main(String[] args) {
-		double Ppr, Tpr, z;
+		double zANN10, zANN5, zDAK;
 		
-		Ppr = 1;
-		Tpr = 1;
 		
-		z = CalculateZFactor.ANN10(Ppr, Tpr);
+		for (double Tpr = 1.0; Tpr <= 3.0; Tpr += 0.1)
+		{
+			System.out.println("\n================================@ Tpr = " + Tpr + " =========================================");
+			for (double Ppr = 0; Ppr <= 30; Ppr += 1.0)
+			{
+				zANN10 = CalculateZFactor.ANN10(Ppr, Tpr);
+				zANN5  = CalculateZFactor.ANN5(Ppr, Tpr);
+				zDAK = CalculateZFactor.DAK(Ppr, Tpr);
+				
+				System.out.println("Ppr = " + Ppr + "\t ==>" + "\tz ANN10 = " + zANN10 + "\tz ANN5 = " + zANN5 + "\tz DAK = " + zDAK);
+				
+			}
+			
+			
+		}
 		
-		System.out.println(z);
+		
+/*		double Ppr = 5.0,
+				Tpr = 2.0;
+		
+		zANN10 = CalculateZFactor.ANN10(Ppr, Tpr);
+		zANN5  = CalculateZFactor.ANN5(Ppr, Tpr);
+		zDAK = CalculateZFactor.DAK(Ppr, Tpr);
+		
+		System.out.println("Ppr = " + Ppr + "\tTpr = " + Tpr + "\n" + "z ANN10 = " + zANN10 + "\tz ANN5 = " + zANN5 + "\tz DAK = " + zDAK);*/
 	}
 }
